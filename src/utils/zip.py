11c86d37c26d95701b79631eacbb3aaf70d4a601
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-06-01 10:31:22 +0100

    Update zip.py

--- src/utils/zip.py
+++ src/utils/zip.py
@@ -1,6 +1,6 @@
 import os
 import shutil
 
-def zip_folder(directory_path: str, output_filename: str):
+def zip(directory_path: str, output_filename: str):
     if os.path.exists(directory_path):
         shutil.make_archive(output_filename if output_filename.endswith(".zip") else output_filename + ".zip", 'zip', directory_path)
