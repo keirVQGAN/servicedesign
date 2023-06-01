a41357d269f4689895880e45f325f54931f882f6
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-06-01 10:44:57 +0100

    Update zip_path.py

--- src/utils/zip_path.py
+++ src/utils/zip_path.py
@@ -1,6 +1,9 @@
 import os
-import shutil
+import zipfile
 
-def zip_path(directory_path: str, output_filename: str):
-    if os.path.exists(directory_path):
-        shutil.make_archive(output_filename.rstrip(".zip"), 'zip', directory_path)
+def zip_path(folder_path: str, output_filename: str, ext: str=None):
+    with zipfile.ZipFile(output_filename.rstrip(".zip") + ".zip", 'w') as zipf:
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                if ext is None or file.endswith(ext):
+                    zipf.write(os.path.join(root, file), arcname=os.path.relpath(os.path.join(root, file), folder_path))
