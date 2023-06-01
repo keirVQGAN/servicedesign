1714afe91e00951e401ead33e5011490a8dc0371
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-06-01 10:38:48 +0100

    Update zip_path.py

--- src/utils/zip_path.py
+++ src/utils/zip_path.py
@@ -1,6 +1,6 @@
 import os
 import shutil
 
-def zip_folder(directory_path: str, output_filename: str):
+def zip_path(directory_path: str, output_filename: str):
     if os.path.exists(directory_path):
         shutil.make_archive(output_filename.rstrip(".zip"), 'zip', directory_path)
