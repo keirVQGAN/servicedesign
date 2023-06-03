116948462c2b099c59e7cb3a0de89d7bc8598826
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-06-03 11:33:55 +0100

    Create txt2str.py

---
+++ src/utils/txt2str.py
@@ -0,0 +1,9 @@
+def txt2str(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            return file.read()
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return None
