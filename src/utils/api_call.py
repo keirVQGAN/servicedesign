cacd1ee5032eaa7962b91f0b09d5ada34975257f
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-06-04 10:59:14 +0100

    Create api_call.py

---
+++ src/utils/api_call.py
@@ -0,0 +1,8 @@
+
+def api_call(url, data=None, headers=None):
+    response = requests.post(url, json=data, headers=headers) if data else requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()  # Assuming the API returns JSON data
+    else:
+        print(f"API call failed with status code: {response.status_code}")
+        return None
