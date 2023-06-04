aafcf1bc39b98a549c177c0fcd45938832a34253
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-06-04 17:07:34 +0100

    Update api_call.py

--- src/utils/api_call.py
+++ src/utils/api_call.py
@@ -1,6 +1,7 @@
 import requests, json, os
 
-def api_call(url, data=None, headers=None):
+def api_call(url, data):
+    headers = {'Content-Type': 'application/json'}
     response = requests.post(url, json=data, headers=headers) if data else requests.get(url, headers=headers)
     if response.status_code == 200:
         return response.json()  # Assuming the API returns JSON data
