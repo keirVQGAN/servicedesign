447bd79ef91a9a57b01ebb11cb4eda1935cd1ea2
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-06-07 21:04:59 +0100

    Update stable.py

--- stable.py
+++ stable.py
@@ -4,14 +4,14 @@ from src.utils.json_utils import update_json_values, yaml2json, save_json_data
 from src.utils.image_utils import upload_img, download_img
 from src.utils.api_call import api_call
 
-class stable:
-    def __init__(self, api_call, prompt, image_file=None, init_image=False):
+class Stable:
+    def __init__(self, api_call, prompt, out_path, image_file=None, init_image=False):
         self.api_call = api_call
         self.prompt = prompt
         self.image_file = image_file
         self.init_image = init_image
         self.in_path = os.environ.get('IN_PATH')
-        self.out_path = os.environ.get('OUT_PATH')
+        self.out_path = out_path
         self.api_key = os.environ.get('STABLE_API_KEY')
         self.base_url = f'https://stablediffusionapi.com/api/v3/{self.api_call}'
         self.config_path = os.environ.get('CONFIG_PATH')
@@ -28,7 +28,7 @@ class stable:
 
         response = api_call(self.base_url, data)
 
-        output_base_path = os.path.join(self.out_path, 'stable', str(response["id"]))
+        output_base_path = os.path.join(self.out_path)
         mkdirs([os.path.join(output_base_path, dir) for dir in ['images', 'json']])
 
         json_files = ['call.json', 'response.json', 'master.json']
@@ -37,3 +37,5 @@ class stable:
             save_json_data(content, file_path, append=(file_name == 'master.json'))
 
         download_img(response["output"], os.path.join(output_base_path, 'images'))
+        
+        return response
