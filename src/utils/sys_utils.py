ccded24ba162cb310e3fba72bf44d6891f7b136f
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-06-09 12:04:45 +0100

    Update sys_utils.py

--- src/utils/sys_utils.py
+++ src/utils/sys_utils.py
@@ -1,5 +1,15 @@
+import os
 from pathlib import Path
 
+
+def path2ls(folder_path, ext='.*'):
+    file_names = []
+    for file in os.listdir(folder_path):
+        if file.endswith(ext):
+            file_name = os.path.splitext(file)[0]
+            file_names.append(file_name)
+    return file_names
+  
 
 def mkdirs(folders):
   for output_folder in folders:
