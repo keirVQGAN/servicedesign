ae79dca835100e512dd652dd3e2027745b518568
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-06-09 12:07:22 +0100

    Update transcript_utils.py

--- src/utils/transcript_utils.py
+++ src/utils/transcript_utils.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import re
 import string
 import nltk
+import json
+import os
 
 nltk.download('punkt')
 nltk.download('stopwords')
@@ -12,6 +14,45 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 
+def save_tutorial(response, student):
+
+    folder_name = f'./output/tutorials/{student}'
+    file_name = f'{folder_name}/{student}.json'
+    # Load existing data or create an empty list
+    try:
+        with open(file_name, 'r') as file:
+            existing_data = json.load(file)
+            if not isinstance(existing_data, list):
+                existing_data = []
+    except (FileNotFoundError, json.JSONDecodeError):
+        existing_data = []
+
+    # Append response and save to json file
+    existing_data.append(response)
+    with open(file_name, 'w') as file:
+        json.dump(existing_data, file, indent=4)
+
+    # Save response text to txt file
+    response_text = response['response']
+    with open(f'{folder_name}/{student}.txt', 'w') as file:
+        file.write(response_text)
+
+    # Append response and student data to master_tutorials.json
+    master_file = './output/tutorials/master_tutorials.json'
+
+    try:
+        with open(master_file, 'r') as file:
+            master_data = json.load(file)
+            if not isinstance(master_data, list):
+                master_data = []
+    except (FileNotFoundError, json.JSONDecodeError):
+        master_data = []
+
+    master_data.append({'student': student, 'response': response})
+    with open(master_file, 'w') as file:
+        json.dump(master_data, file, indent=4)
+
+        
 def clean_text(input_file, output_file, student):
     with open(input_file, 'r') as file:
         content = file.readlines()
@@ -44,6 +85,8 @@ def clean_text(input_file, output_file, student):
         file.write(cleaned_text)
 
     return cleaned_text
+
+
 def txt2docx(input_path):
     for txt_file in Path(input_path).rglob('*.txt'):
         if 'clean' in txt_file.name: continue
