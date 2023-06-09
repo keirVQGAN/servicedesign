34c4fc1bd2238ee20700bbcc2945159b526f76d7
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-06-09 12:07:56 +0100

    Update sys_utils.py

--- src/utils/sys_utils.py
+++ src/utils/sys_utils.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from docx import Document
+import zipfile
 
 
 def docx2txt(docx_path):
@@ -27,4 +28,12 @@ def mkdirs(folders):
   for output_folder in folders:
     output_folder = Path(output_folder)
     output_folder.mkdir(parents=True, exist_ok=True)
+    
+
+def zip_path(folder_path: str, output_filename: str, ext: str=None):
+    with zipfile.ZipFile(output_filename.rstrip(".zip") + ".zip", 'w') as zipf:
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                if ext is None or file.endswith(ext):
+                    zipf.write(os.path.join(root, file), arcname=os.path.relpath(os.path.join(root, file), folder_path))
     
