495f98a53b5e23f4ab6b5c55e3bdcfd657a81b4d
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-06-09 12:06:21 +0100

    Update sys_utils.py

--- src/utils/sys_utils.py
+++ src/utils/sys_utils.py
@@ -1,5 +1,17 @@
 import os
 from pathlib import Path
+from docx import Document
+
+
+def docx2txt(docx_path):
+    doc = Document(docx_path)
+    text = '\n'.join([paragraph.text for paragraph in doc.paragraphs])
+    txt_path = os.path.splitext(docx_path)[0] + '.txt'
+
+    with open(txt_path, 'w') as txt_file:
+        txt_file.write(text)
+
+    return txt_path
 
 
 def path2ls(folder_path, ext='.*'):
