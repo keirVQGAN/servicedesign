b0baa41076fbd1e785c3d71bfb81395491caca30
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-06-11 10:11:58 +0100

    Update image_utils.py

--- src/utils/image_utils.py
+++ src/utils/image_utils.py
@@ -1,7 +1,27 @@
 import os
 import requests
+import json
 from pyuploadcare import Uploadcare, File
 
+
+def super(key, img_url, scale, face_enhance):
+    api_url = "https://stablediffusionapi.com/api/v3/super_resolution"
+    payload = json.dumps({
+        "key": key,
+        "url": img_url,
+        "scale": scale,
+        "webhook": None,
+        "face_enhance": face_enhance
+    })
+
+    response = requests.post(api_url, headers={'Content-Type': 'application/json'}, data=payload)
+    response_data = response.json()
+
+    link = response_data.get('output', 'No link found')
+
+    return response_data, link
+
+  
 def upload_img(img_path):
   public_key = os.environ.get('UCARE_API_KEY_PUBLIC')
   secret_key = os.environ.get('UCARE_API_KEY_SECRET')
