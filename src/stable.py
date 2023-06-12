1cc93a7cf353fa5010ed58f83f8727c89bc4f265
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-06-12 16:27:22 +0100

    Update stable.py

--- src/stable.py
+++ src/stable.py
@@ -1,8 +1,12 @@
 import requests
 import yaml
 import time
+import json
 
-def stable(api_key=None, call=None, prompt=None, negative_prompt=None, init_image=None, mask_image=None):
+def stable(api_key=None, call=None, prompt=None, negative_prompt=None, init_image=None, mask_image=None,
+           enhance_prompt=None, guidance_scale=None, height=None, width=None, samples=None,
+           num_inference_steps=None, self_attention=None, upscale=None, seed=None):
+    
     if call == 'controlnet':
         url = 'https://stablediffusionapi.com/api/v5/controlnet'
         yaml_file = '/content/config/stable/controlnet.yml'
@@ -18,7 +22,16 @@ def stable(api_key=None, call=None, prompt=None, negative_prompt=None, init_imag
         'key': api_key,
         'negative_prompt': negative_prompt,
         'init_image': init_image or api_options.get('init_image'),
-        'mask_image': mask_image or api_options.get('mask_image')
+        'mask_image': mask_image or api_options.get('mask_image'),
+        'enhance_prompt': enhance_prompt or api_options.get('enhance_prompt'),
+        'guidance_scale': guidance_scale or api_options.get('guidance_scale'),
+        'height': height or api_options.get('height'),
+        'width': width or api_options.get('width'),
+        'samples': samples or api_options.get('samples'),
+        'num_inference_steps': num_inference_steps or api_options.get('num_inference_steps'),
+        'self_attention': self_attention or api_options.get('self_attention'),
+        'upscale': upscale or api_options.get('upscale'),
+        'seed': seed or api_options.get('seed')
     })
 
     headers = {"Content-Type": "application/json"}
@@ -31,4 +44,3 @@ def stable(api_key=None, call=None, prompt=None, negative_prompt=None, init_imag
         return None
 
     return response_data
-
