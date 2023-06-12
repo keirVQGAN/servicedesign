fe16723f5aa59480e7770c09ab4c86032e7dbf12
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-06-12 17:43:51 +0100

    Update stable.py

--- stable.py
+++ stable.py
@@ -1,41 +1,60 @@
-import os
-from src.utils.sys_utils import mkdirs
-from src.utils.json_utils import update_json_values, yaml2json, save_json_data
-from src.utils.image_utils import upload_img, download_img
-from src.utils.api_call import api_call
+import requests
+import yaml
+import time
+import json
+import itertools
+from response_processor import ResponseProcessor
 
-class Stable:
-    def __init__(self, api_call, prompt, out_path, image_file=None, init_image=False):
-        self.api_call = api_call
-        self.prompt = prompt
-        self.image_file = image_file
-        self.init_image = init_image
-        self.in_path = os.environ.get('IN_PATH')
-        self.out_path = out_path
-        self.api_key = os.environ.get('STABLE_API_KEY')
-        self.base_url = f'https://stablediffusionapi.com/api/v3/{self.api_call}'
-        self.config_path = os.environ.get('CONFIG_PATH')
-        self.config_file_path = os.path.join(self.config_path, 'stable', f'{self.api_call}.yml')
+class StableAPI:
 
-    def process(self):
-        image_url = ""
-        if self.init_image and self.image_file:
-            image_path = os.path.join(self.in_path, 'images', 'init_images', self.image_file)
-            image_url = upload_img(image_path)
+    BASE_URL = 'https://stablediffusionapi.com/api'
+    CONFIG_PATH = './config/stable'
+    HEADERS = {"Content-Type": "application/json"}
 
-        data = yaml2json(self.config_file_path)
-        update_json_values(data, {'key': self.api_key, 'prompt': self.prompt, 'init_image': image_url})
+    def __init__(self, api_key=None):
+        self.api_key = api_key
 
-        response = api_call(self.base_url, data)
+    @staticmethod
+    def _load_yaml(file):
+        with open(file, 'r') as f:
+            return yaml.safe_load(f)
 
-        output_base_path = os.path.join(self.out_path)
-        mkdirs([os.path.join(output_base_path, dir) for dir in ['images', 'json']])
+    def _make_request(self, url, json_body):
+        response = requests.post(url=url, headers=self.HEADERS, json=json_body)
+        try:
+            return response.json()
+        except json.JSONDecodeError:
+            print(f"Failed to parse JSON from response. Status code: {response.status_code}, Response text: {response.text}")
+            return None
 
-        json_files = ['call.json', 'response.json', 'master.json']
-        for file_name, content in zip(json_files, [data, response, response]):
-            file_path = os.path.join(output_base_path, 'json', f'{response["id"]}_{file_name}')
-            save_json_data(content, file_path, append=(file_name == 'master.json'))
+    def request(self, call=None, **kwargs):
+        url = f'{self.BASE_URL}/v5/controlnet' if call == 'controlnet' else f'{self.BASE_URL}/v3/{call}'
+        yaml_file = f'{self.CONFIG_PATH}/controlnet.yml' if call == 'controlnet' else f'{self.CONFIG_PATH}/{call}.yml'
 
-        download_img(response["output"], os.path.join(output_base_path, 'images'))
+        api_options = self._load_yaml(yaml_file)
+        api_options.update(kwargs)
+        api_options['key'] = self.api_key
+
+        return self._make_request(url, api_options)
+
+    def get_responses(self, options_dict, debug=False):
+        keys, values = zip(*options_dict.items())
+        combos = [dict(zip(keys, v)) for v in itertools.product(*values)]
+
+        responses = []
+        for combo in combos:
+            response_data = self.request(**combo)
+            if debug:
+                self.debug_message(combo, response_data)
+            responses.append(response_data)
         
-        return response
+        return responses
+
+    @staticmethod
+    def debug_message(combo, response_data):
+        print('Rendering: ' + str(combo))
+        status = response_data['status']
+        if status == 'success':
+            print(str(response_data['output']) + '\n')
+        elif status == 'processing':
+            print('Processing Image. Run fetch after ' + str(round(float(response_data['eta']), 2)) + ' sec.\n')
