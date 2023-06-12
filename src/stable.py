ad512fe96c9b697921ebc68c65775d89fad14f44
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-06-12 17:02:01 +0100

    Update stable.py

--- src/stable.py
+++ src/stable.py
@@ -3,44 +3,48 @@ import yaml
 import time
 import json
 
-def stable(api_key=None, call=None, prompt=None, negative_prompt=None, init_image=None, mask_image=None,
-           enhance_prompt=None, guidance_scale=None, height=None, width=None, samples=None,
-           num_inference_steps=None, self_attention=None, upscale=None, seed=None):
-    
-    if call == 'controlnet':
-        url = 'https://stablediffusionapi.com/api/v5/controlnet'
-        yaml_file = '/content/config/stable/controlnet.yml'
-    else:
-        url = f'https://stablediffusionapi.com/api/v3/{call}'
-        yaml_file = f'./config/stable/{call}.yml'
+class StableAPI:
+    def __init__(self, api_key=None):
+        self.api_key = api_key
 
-    with open(yaml_file, 'r') as f:
-        api_options = yaml.safe_load(f)
+    def request(self, call=None, prompt=None, negative_prompt=None, init_image=None, mask_image=None,
+                enhance_prompt=None, guidance_scale=None, height=None, width=None, samples=None,
+                num_inference_steps=None, self_attention=None, upscale=None, seed=None):
+        
+        if call == 'controlnet':
+            url = 'https://stablediffusionapi.com/api/v5/controlnet'
+            yaml_file = '/content/config/stable/controlnet.yml'
+        else:
+            url = f'https://stablediffusionapi.com/api/v3/{call}'
+            yaml_file = f'./config/stable/{call}.yml'
 
-    api_options.update({
-        'prompt': prompt,
-        'key': api_key,
-        'negative_prompt': negative_prompt,
-        'init_image': init_image or api_options.get('init_image'),
-        'mask_image': mask_image or api_options.get('mask_image'),
-        'enhance_prompt': enhance_prompt or api_options.get('enhance_prompt'),
-        'guidance_scale': guidance_scale or api_options.get('guidance_scale'),
-        'height': height or api_options.get('height'),
-        'width': width or api_options.get('width'),
-        'samples': samples or api_options.get('samples'),
-        'num_inference_steps': num_inference_steps or api_options.get('num_inference_steps'),
-        'self_attention': self_attention or api_options.get('self_attention'),
-        'upscale': upscale or api_options.get('upscale'),
-        'seed': seed or api_options.get('seed')
-    })
+        with open(yaml_file, 'r') as f:
+            api_options = yaml.safe_load(f)
 
-    headers = {"Content-Type": "application/json"}
-    response = requests.post(url=url, headers=headers, json=api_options)
+        api_options.update({
+            'prompt': prompt,
+            'key': self.api_key,
+            'negative_prompt': negative_prompt,
+            'init_image': init_image or api_options.get('init_image'),
+            'mask_image': mask_image or api_options.get('mask_image'),
+            'enhance_prompt': enhance_prompt or api_options.get('enhance_prompt'),
+            'guidance_scale': guidance_scale or api_options.get('guidance_scale'),
+            'height': height or api_options.get('height'),
+            'width': width or api_options.get('width'),
+            'samples': samples or api_options.get('samples'),
+            'num_inference_steps': num_inference_steps or api_options.get('num_inference_steps'),
+            'self_attention': self_attention or api_options.get('self_attention'),
+            'upscale': upscale or api_options.get('upscale'),
+            'seed': seed or api_options.get('seed')
+        })
 
-    try:
-        response_data = response.json()
-    except json.JSONDecodeError:
-        print(f"Failed to parse JSON from response. Status code: {response.status_code}, Response text: {response.text}")
-        return None
+        headers = {"Content-Type": "application/json"}
+        response = requests.post(url=url, headers=headers, json=api_options)
 
-    return response_data
+        try:
+            response_data = response.json()
+        except json.JSONDecodeError:
+            print(f"Failed to parse JSON from response. Status code: {response.status_code}, Response text: {response.text}")
+            return None
+
+        return response_data
