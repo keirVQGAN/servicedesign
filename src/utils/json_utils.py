bcc042fdc16703a2bfdb60353e745b61b2df2c2b
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-06-12 10:26:46 +0100

    Update json_utils.py

--- src/utils/json_utils.py
+++ src/utils/json_utils.py
@@ -15,14 +15,10 @@ def write_to_json(data, path):
     with open(path, 'w') as f:
         json.dump(data, f, indent=4, separators=(',', ': '))
 
+        
 def append_to_json(data, path):
-    existing_data = []
-    if os.path.exists(path):
-        with open(path, 'r') as f:
-            existing_data = [json.loads(line) for line in f]
-    if data not in existing_data:
-        with open(path, 'a') as f:
-            f.write(json.dumps(data, indent=4, separators=(',', ': ')) + "\n")
+    with open(path, 'a') as f:
+        json.dump(data, f, indent=4, separators=(',', ': '))
 
 
         
